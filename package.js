753d4dc2f319eb86dfdd57c69c474f919b63561d
Eric Wyne <ecwyne@gmail.com>
2015-04-28 16:53:11 -0700

    new package system

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
-  summary: "Mathjs, An extensive math library for JavaScript, packaged for Meteor. See http://mathjs.org."
+  summary: "Mathjs, An extensive math library for JavaScript, packaged for Meteor. See http://mathjs.org.",
   version: '1.6.0_1',
-  git: 'https://github.com/ecwyne/meteor-mathjs'
+  git: 'https://github.com/ecwyne/meteor-mathjs',
+  name: 'ecwyne:mathjs'
 });
 
 Package.on_use(function (api, where) {
