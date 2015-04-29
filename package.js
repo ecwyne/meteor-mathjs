558115c84c5f6a0c76bdcc32bb30b048bc4b7d20
Eric Wyne <ecwyne@gmail.com>
2015-04-29 09:41:37 -0700

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "Mathjs, An extensive math library for JavaScript, packaged for Meteor. See http://mathjs.org.",
-  version: '1.6.0_1',
+  summary: 'Mathjs, An extensive math library for JavaScript, packaged for Meteor. See http://mathjs.org.',
+  version: '1.6.0_2',
   git: 'https://github.com/ecwyne/meteor-mathjs',
   name: 'ecwyne:mathjs'
 });
