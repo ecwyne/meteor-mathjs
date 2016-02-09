3fb66c683927e0f3ad1ac2c28a6f16c7981a32b5
Eric Wyne <ecwyne@gmail.com>
2016-02-09 08:50:22 -0800

    update to 2.7.0 closes #6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Mathjs, An extensive math library for JavaScript, packaged for Meteor. See http://mathjs.org.',
-  version: '2.4.0',
+  version: '2.7.0',
   git: 'https://github.com/ecwyne/meteor-mathjs',
   name: 'ecwyne:mathjs'
 });
